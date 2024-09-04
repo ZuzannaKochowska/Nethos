1cbdd424fe8b952e88eaebd807db1a8faeb52b6f
ZuzannaKochowska <zuzannakochowska@gmail.com>
2024-09-04 22:37:14 +0200

    test

---
+++ Nethos/landing/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import { Headline } from './Headline';
+
+function App() {
+  return (
+    <div className="App">
+      Hello
+      <Headline />
+    </div>
+  );
+}
+
+export default App;
---
+++ Nethos/landing/src/Headline/index.jsx
@@ -0,0 +1,6 @@
+import './styles.css'
+
+export function Headline() {
+    return(
+    <div><h1>Welcome to Nethos</h1></div>)
+}

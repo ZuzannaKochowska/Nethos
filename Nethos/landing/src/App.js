bc4940d0809d949dd7d2f158ae375a45397084bb
ZuzannaKochowska <zuzannakochowska@gmail.com>
2024-09-05 17:04:20 +0200

    Services

--- Nethos/landing/src/App.js
+++ Nethos/landing/src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import { Headline } from './Headline';
+import { Services } from './Services';
 
 function App() {
   return (
     <div className="App">
       
       <Headline />
+      <Services />
     </div>
   );
 }

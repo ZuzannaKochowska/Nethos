287df4cb68c50fbe46023a4dde5a1d1d5ee866a6
ZuzannaKochowska <zuzannakochowska@gmail.com>
2024-09-05 23:28:06 +0200

    remarks

--- Nethos/landing/src/Why/index.jsx
+++ Nethos/landing/src/Why/index.jsx
@@ -7,16 +7,18 @@ export function Why() {
         <div className="why">
             Dlaczego wybrać Nethos?
         </div>
-        <div className="lista">
+        <div className="lista-container">
+            <ul className='lista'>
 
         <li>Doświadczony zespół specjalistów </li>
         <li>Indywidualne podejście</li>    
         <li>Nowoczesne technologie</li>
-        <li>Bezpieczeństwo danych</li>
+        <li>Bezpieczeństwo danych</li> </ul>
+        
         </div>
 
        <div className='button-container'> 
-        <button className="button">Napisz do nas!</button>
+        <button className="button">NAPISZ DO NAS!</button>
        </div></div>
     )
 }
